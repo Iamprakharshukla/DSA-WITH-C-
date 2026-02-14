3111c27cb7244bcefca11652448d2865b7357773
Prakhar Shukla <prakharshukla297@gmail.com>
2026-02-14 22:23:58 +0530

    Day 3

---
+++ DAY-3/nestedifelse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int age = 20;
+int id = 1;
+int main() {
+if(age >= 18) {
+    if(id == 1) {
+        cout << "Allowed";
+    }
+}
+else {
+    cout << "Not allowed";
+}
+return 0;
+}
